5a319a4fda6fe91458cb24669b9996e6a0c3b1c2
Kevin Santos <kevinochieng548@gmail.com>
2019-02-13 19:51:17 +0300

    extract the exif gps data and write to csv file

--- app.rb
+++ app.rb
@@ -4,3 +4,11 @@ require 'csv'
 jpg_files = Dir["./*.jpg"]
 puts jpg_files
 
+CSV.open('cli_exif.csv', 'wb' ) do |row|
+  jpg_files.each do |f|
+    longitude = Exif::Data.new(IO.read(f)).gps_longitude 
+    latitude = Exif::Data.new(IO.read(f)).gps_latitude
+    row << [f, longitude, latitude]
+  end
+end
+
